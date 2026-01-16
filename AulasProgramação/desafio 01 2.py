6f0e51baebedee3f798bc24436c65e77cad72af7
M0rckks <matsumatsu0880@gmail.com>
2026-01-15 23:52:44 -0300

    Add files via upload

---
+++ AulasProgramação/desafio 01 2.py
@@ -0,0 +1,6 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> nome = input('Qual o seu nome?')
+Qual o seu nome? Morckks
+>>> print('Olá Morckks! Seja bem vindo ao nosso sistema')
+Olá Morckks! Seja bem vindo ao nosso sistema
---
+++ AulasProgramação/teste 01.py
@@ -0,0 +1,4 @@
+nome = input('Qual é o seu nome?')
+idade = input('Quantos anos vc tem?')
+hobby = input('Qual o seu hobby?')
+print(nome, idade, hobby)
